85570c180cd2de230339788735de3a5591114ee0
nguyenhuyhoanlc <nguyenhuyhoanlc@gmail.com>
2020-12-09 15:29:20 +0700

    Bài Tập

---
+++ SolveTheEquation.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SolveTheEquation {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants: ");
+        Scanner s = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = s.nextDouble();
+        System.out.println("b: ");
+        double b = s.nextDouble();
+        System.out.println("c: ");
+        double c = s.nextDouble();
+
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("The solution is: %f!", solution);
+
+            if (a != 0) {
+                double answer = (c - b) / a;
+                System.out.printf("Equation pass with x = %f!\n", answer);
+            } else {
+                if (b == 0) {
+                    System.out.print("The solution is all x!");
+                } else {
+                    System.out.print("No solution!");
+                }
+            }
+        }
+    }
+}
